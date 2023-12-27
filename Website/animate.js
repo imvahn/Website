a9077f494d9aec5423cd03414a8bd50253a5cca6
imvahn <144669986+imvahn@users.noreply.github.com>
2023-12-27 02:05:17 -0500

    oops fucked it up it's fixed now

---
+++ Website/animate.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    reset: true,
+    distance: '10%',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.section-title', { delay: 300, origin: 'left', interval: 800 });
+ScrollReveal().reveal('.info-title', { delay: 400, origin: 'left', interval: 200 });
+ScrollReveal().reveal('.info', { delay: 500, origin: 'bottom', interval: 250 });
--- Website/test.js
+++ Website/test.js
@@ -1,20 +1,10 @@
-// Create the cursor element outside of section-01
-const cursor = document.createElement("div");
-cursor.classList.add("circle");
-document.body.appendChild(cursor);
+const cursor = document.querySelector(".circle");
 
-// Function to update cursor position
 function getDimensions(e) {
     cursor.style.top = `${e.clientY - 25}px`; // -25px for the size of the circle
     cursor.style.left = `${e.clientX - 25}px`;
 }
 
-// Add mousemove event listener to the entire document
-document.addEventListener("mousemove", (e) => {
-    getDimensions(e);
-});
-
-// Add scroll event listener to update the cursor position
-window.addEventListener("scroll", (e) => {
+window.addEventListener("mousemove", (e) => {
     getDimensions(e);
 });
