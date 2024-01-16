437b1c499405dda3a4df69b49bd40a9e049e9ba0
Vahn <vahnkess@gmail.com>
2024-01-15 23:29:40 -0500

    website is fully responsive for mobile

--- public/tabs.js
+++ public/tabs.js
@@ -7,6 +7,32 @@ let heights = [];
 let dur = 0.4;
 let animation;
 
+// Function to update offsetWidth, offsetLeft and offsetTop values
+function updateOffsets() {
+    if (!window.matchMedia("(max-aspect-ratio: 11/9)").matches) {
+        gsap.set(".slider", { x: targets[activeTab].offsetLeft, y: targets[activeTab].offsetTop - 17, width: targets[activeTab].offsetWidth, height: targets[activeTab].offsetHeight });
+    }
+    else {
+        gsap.set(".slider", { x: targets[activeTab].offsetLeft, y: targets[activeTab].offsetTop - 17, width: targets[activeTab].offsetWidth, height: targets[activeTab].offsetHeight });
+    }
+}
+
+
+// Function to update the height of the articles
+function updateArticles() {
+    heights = [];
+    for (let i = 0; i < targets.length; i++) {
+        heights.push(articles[i].offsetHeight); // get height of each article
+        gsap.set(articles[i], { top: 0, y: -heights[i] }); // push all articles up out of view
+    }
+    gsap.set(articles[activeTab], { y: 0 });
+    gsap.set(".article-block", { height: heights[activeTab] });
+}
+
+function updateTargets() {
+    targets = document.querySelectorAll("li");
+}
+
 for (let i = 0; i < targets.length; i++) {
     targets[i].index = i;
     heights.push(articles[i].offsetHeight); // get height of each article
@@ -16,9 +42,16 @@ for (let i = 0; i < targets.length; i++) {
 
 // set initial article and position bubble slider on first tab 
 gsap.set(articles[0], { y: 0 });
-gsap.set(".slider", { x: targets[0].offsetLeft, width: targets[0].offsetWidth });
+gsap.set(".slider", { x: targets[0].offsetLeft, width: targets[0].offsetWidth, height: targets[0].offsetHeight });
 gsap.set(targets[0], { color: "#fff" });
 gsap.set(".article-block", { height: heights[0] });
+
+// Event listener for window resize
+window.addEventListener("resize", function () {
+    updateTargets();
+    updateOffsets();
+    updateArticles();
+});
 
 function doCoolStuff() {
     // check if clicked target is new and if the timeline is currently active
@@ -31,7 +64,12 @@ function doCoolStuff() {
         old = activeTab;
         activeTab = this.index;
         // animate bubble slider to clicked target
-        animation.to(".slider", { x: targets[activeTab].offsetLeft, width: targets[activeTab].offsetWidth });
+        if (!window.matchMedia("(max-aspect-ratio: 11/9)").matches) {
+            animation.to(".slider", { x: targets[activeTab].offsetLeft, width: targets[activeTab].offsetWidth });
+        }
+        else {
+            animation.to(".slider", { y: targets[activeTab].offsetTop - 17, height: targets[activeTab].offsetHeight });
+        }
         // change text color on old and new tab targets
         animation.to(targets[old], { color: "black", ease: "none" }, 0);
         animation.to(targets[activeTab], { color: "#fff", ease: "none" }, 0);
