fe23cea44761ea548cfc30bf0f16caa5d3ce1e64
imvahn <144669986+imvahn@users.noreply.github.com>
2024-03-17 02:39:01 -0400

    tiny

--- public/animate.js
+++ public/animate.js
@@ -1,12 +1,18 @@
 ScrollReveal({
     reset: false,
+    distance: '50%',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.container', { delay: 400, origin: 'bottom', interval: 200 });
+
+ScrollReveal({
+    reset: false,
     distance: '10%',
     duration: 2500,
     delay: 400
 });
 
-ScrollReveal().reveal('.section-title', { delay: 300, origin: 'left', interval: 200 });
-ScrollReveal().reveal('.info-title', { delay: 400, origin: 'left', interval: 200 });
-ScrollReveal().reveal('.info', { delay: 500, origin: 'bottom', interval: 250 });
-ScrollReveal().reveal('.info-2', { delay: 700, origin: 'bottom', interval: 100 });
-ScrollReveal().reveal('.info-3', { delay: 900, origin: 'bottom', interval: 100 });
+ScrollReveal().reveal('.education-item', { delay: 400, origin: 'left', interval: 200 });
+ScrollReveal().reveal('.about-me', { delay: 400, origin: 'right', interval: 200 });
